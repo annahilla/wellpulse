a06f189086efc01149ecf8209c6b48cf0e952337
Anna Hilla <annahilla21@gmail.com>
2024-12-15 20:17:13 +0100

    Style scrollbar

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -31,12 +31,12 @@ const Modal = ({ isOpen, closeModal, children }: ModalProps) => {
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
         <div
           ref={modalRef}
-          className="w-11/12 max-w-2xl relative bg-white p-10 rounded-xl flex flex-col items-center justify-start lg:w-1/2 max-h-[90vh] overflow-y-auto"
+          className="w-11/12 max-w-2xl relative bg-white p-10 rounded-xl flex flex-col items-center justify-start lg:w-1/2 max-h-[90vh]"
         >
           <button className="absolute right-5 top-5" onClick={closeModal}>
             <IoMdClose />
           </button>
-          <div className="overflow-y-auto max-h-[calc(100vh-4rem)] w-full p-4">
+          <div className="overflow-y-auto w-full p-4 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
             {children}
           </div>
         </div>
